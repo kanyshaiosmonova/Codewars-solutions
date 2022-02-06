b4ff6ac9333e663600a5bb6e331e409c3a221228
Kanyshai Osmonova <osmonova.kanyshai@gmail.com>
2022-02-06 20:36:54 +0600

    Nickname generator

--- nicknamegenerator.js
+++ nicknamegenerator.js
@@ -20,3 +20,8 @@ Vowels are "aeiou", so discount the letter "y".
 Input will always be a string.
 Input will always have the first letter capitalised and the rest lowercase (e.g. Sam).
 The input can be modified */
+function nicknameGenerator(name){
+    if (name.length<4) return "Error: Name too short";
+    if (name[2].match(/[aeuio]/)) return name.slice(0,4)
+    return name.slice(0,3)
+  }
