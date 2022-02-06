98b908ab962cc5ba3d19c9358ce3f8c71d2b7937
Kanyshai Osmonova <osmonova.kanyshai@gmail.com>
2022-02-06 20:35:16 +0600

    My languages

--- mylanguages.js
+++ mylanguages.js
@@ -7,3 +7,7 @@ Examples
 {"Java": 10, "Ruby": 80, "Python": 65}    -->  ["Ruby", "Python"]
 {"Hindi": 60, "Dutch" : 93, "Greek": 71}  -->  ["Dutch", "Greek", "Hindi"]
 {"C++": 50, "ASM": 10, "Haskell": 20}     -->  [] */
+
+function myLanguages(results) {
+    return Object.keys(results).filter(v=>results[v]>=60).sort((a,b)=>results[b]-results[a])
+}
